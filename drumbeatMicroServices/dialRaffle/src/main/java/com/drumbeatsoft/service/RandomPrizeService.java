4639b218a81273cc335127e2d7e05b9dcc66a350
184514999 <184514999@qq.com>
2019-02-15 14:07:49 +0800

    14：07

--- drumbeatMicroServices/dialRaffle/src/main/java/com/drumbeatsoft/service/RandomPrizeService.java
+++
@@ -1,9 +0,0 @@
-package com.drumbeatsoft.service;
-
-import com.drumbeatsoft.common.ResponseResult;
-import com.drumbeatsoft.model.Prize;
-
-public interface RandomPrizeService {
-
-    ResponseResult<Prize> getRandomPrize(String activityId,String accountId);
-}
--- drumbeatMicroServices/dialRaffle/src/main/java/com/drumbeatsoft/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.drumbeatsoft.service;
-
-import com.drumbeatsoft.model.UserInfo;
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface UserService extends UserDetailsService {
-
-}
